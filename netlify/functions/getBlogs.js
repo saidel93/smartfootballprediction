3c999ab9699fae29b63468d69b3a8b8d2979e743
saidel93 <saidelkhezraji@gmail.com>
2026-02-19 10:23:14 -0600

    Update getBlogs.js

--- netlify/functions/getBlogs.js
+++ netlify/functions/getBlogs.js
@@ -1,37 +1,21 @@
-// netlify/functions/getBlogs.js
-// URL: /.netlify/functions/getBlogs?limit=12&category=weekend-picks
-// URL: /.netlify/functions/getBlogs?slug=some-blog-slug
-const { connectToDatabase } = require('./utils/mongodb');
+const { MongoClient } = require("mongodb");
 
-const H = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' };
+exports.handler = async function () {
 
-exports.handler = async (event) => {
-  if (event.httpMethod === 'OPTIONS') return { statusCode: 200, headers: H, body: '' };
-  try {
-    const { db } = await connectToDatabase();
-    const p = event.queryStringParameters || {};
+  const client = new MongoClient(process.env.MONGODB_URI);
+  await client.connect();
+  const db = client.db("smartfootball");
 
-    // Single blog
-    if (p.slug) {
-      const blog = await db.collection('blogs').findOne({ slug: p.slug, isPublished: true });
-      if (!blog) return { statusCode: 404, headers: H, body: JSON.stringify({ error: 'Blog not found' }) };
-      return { statusCode: 200, headers: H, body: JSON.stringify({ success: true, blog: { ...blog, _id: blog._id?.toString() } }) };
-    }
+  const blogs = await db.collection("blogs")
+    .find({})
+    .sort({ date: -1 })
+    .limit(5)
+    .toArray();
 
-    // Blog listing
-    const query = { isPublished: true };
-    if (p.category && p.category !== 'all') query.category = p.category;
-    const limit = Math.min(parseInt(p.limit) || 12, 50);
-    const skip  = parseInt(p.page || 0) * limit;
+  await client.close();
 
-    const [blogs, total] = await Promise.all([
-      db.collection('blogs').find(query, { projection: { content: 0 } }).sort({ createdAt: -1 }).skip(skip).limit(limit).toArray(),
-      db.collection('blogs').countDocuments(query),
-    ]);
-
-    return { statusCode: 200, headers: H, body: JSON.stringify({ success: true, blogs: blogs.map(b => ({ ...b, _id: b._id?.toString() })), total, hasMore: skip + limit < total }) };
-  } catch (e) {
-    console.error('getBlogs:', e);
-    return { statusCode: 500, headers: H, body: JSON.stringify({ error: e.message }) };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ blogs }),
+  };
 };
